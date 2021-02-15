3e0aad2257403c7e39653ca70d7d79fd4655edd2
gdaneti <gdaneti@cisco.com>
2021-02-15 17:52:40 +0530

    Create git pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  client_id: 'Ca9ad2e942866e1d350c38a2023e422f769125d13b97c6614d52c60b44b7f0df8',
+  client_secret: '',
+  redirect_uri: 'http://localhost:4200',
+  scope: 'spark:all spark:kms'
 };
